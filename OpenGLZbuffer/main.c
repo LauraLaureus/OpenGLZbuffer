95c3804ba4ef0c549f176997e6dae5c1871dfd58
Laura <l4depende@gmail.com>
2016-03-08 09:44:52 +0000

    ScreenShots+Code

--- OpenGLZbuffer/main.c
+++ OpenGLZbuffer/main.c
@@ -1,15 +1,32 @@
 //
 //  main.c
-//  OpenGLZbuffer
+//  OpenGLPerspective
 //
-//  Created by Laura del Pino Díaz on 8/3/16.
+//  Created by Laura del Pino Díaz on 7/3/16.
 //  Copyright © 2016 Laura del Pino Díaz. All rights reserved.
 //
 
 #include <stdio.h>
+#include "View.h"
+
+
+int w_width = 500,w_height = 500;
+float gl_width = 2.0,gl_height = 2.0,gl_near = 2.0,gl_far =4.0;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    glutInit(&argc, argv);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(w_width
+                       , w_height);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE | GLUT_DEPTH);
+    
+    glutCreateWindow("OpenGL Practica ");
+    Init();
+    glutDisplayFunc(Display); // registra la funcion de rendering
+    glutReshapeFunc(ReshapeSize);
+    
+    glutMainLoop(); // bucle principal
+    
     return 0;
 }
